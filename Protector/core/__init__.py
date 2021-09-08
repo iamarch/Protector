1bcad5f757d738e12dac2c8e4327faea399aed7b
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-09-08 22:33:21 +0530

    Create __init__.py

---
+++ Protector/core/__init__.py
@@ -0,0 +1,20 @@
+"""Bot core initialization"""
+# Protector (A telegram bot project)
+# Copyright (C) 2021 - Kunaldiwan All rights reserved. Source code available under the AGPL.
+
+# This file is part of Protector.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+from .Protector import Protector
+from .logging import *
