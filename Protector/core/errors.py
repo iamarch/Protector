52e03019a5beb7377991c78f8e7cd664b21e736e
Kunal Diwan <86975698+Kunal-Diwan@users.noreply.github.com>
2021-09-08 22:53:10 +0530

    Changed License #2

--- Protector/core/errors.py
+++ Protector/core/errors.py
@@ -1,19 +1,21 @@
 """Protector Errors Constructor"""
-# Copyright (C) 2020 - 2021  UserbotIndo Team, <https://github.com/userbotindo.git>
-#
+# Protector (A telegram bot project)
+# Copyright (C) 2021 - Kunaldiwan All rights reserved. Source code available under the AGPL.
+
+# This file is part of Protector.
+
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# GNU Affero General Public License for more details.
 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
 
 __all__ = ["ProtectorException", "BackupError", "CommandInvokeError", "UnknownUpdateError"]
 
